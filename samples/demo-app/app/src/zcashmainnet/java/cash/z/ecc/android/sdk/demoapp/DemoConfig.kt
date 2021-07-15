6d01ef690a197ed07eb4dad40655e0773e990126
Satinder Grewal <grewal@lavabit.com>
2021-07-16 02:18:33 +1200

    use compiled aar from sdk in demo app

--- samples/demo-app/app/src/zcashmainnet/java/cash/z/ecc/android/sdk/demoapp/DemoConfig.kt
+++ samples/demo-app/app/src/zcashmainnet/java/cash/z/ecc/android/sdk/demoapp/DemoConfig.kt
@@ -2,15 +2,15 @@ package cash.z.ecc.android.sdk.demoapp
 
 data class DemoConfig(
     val alias: String = "SdkDemo",
-    val host: String = "mainnet.lightwalletd.com",
-    val port: Int = 9067,
-    val birthdayHeight: Int = 968000,
-    val utxoEndHeight: Int = 968085,
+    val host: String = "lightd.pirate.black",
+    val port: Int = 443,
+    val birthdayHeight: Int = 1390000,
+    val utxoEndHeight: Int = 1476903,
     val sendAmount: Double = 0.000018,
 
     // corresponds to address: zs15tzaulx5weua5c7l47l4pku2pw9fzwvvnsp4y80jdpul0y3nwn5zp7tmkcclqaca3mdjqjkl7hx
-    val initialSeedWords: String = "wish puppy smile loan doll curve hole maze file ginger hair nose key relax knife witness cannon grab despair throw review deal slush frame",
+    val initialSeedWords: String = "eyebrow luggage boy enemy stamp lunch middle slab mother bacon confirm again tourist idea grain pink angle comic question rabbit pole train dragon grape",
 
-    // corresponds to seed: urban kind wise collect social marble riot primary craft lucky head cause syrup odor artist decorate rhythm phone style benefit portion bus truck top
-    val toAddress: String = "zs1lcdmue7rewgvzh3jd09sfvwq3sumu6hkhpk53q94kcneuffjkdg9e3tyxrugkmpza5c3c5e6eqh"
+    // corresponds to seed: 
+    val toAddress: String = "zs163xv6jsmpuyrlzr58glzv9aetepcyss5n3pqg9gqzahf5aghd65za99zult82dxmx2qsjswxumj"
 ) 
